ee679d2c6fd46f6d191270466f880c9a822b5503
Nkosingiphile Nkosi <nknkosi@c4r16s5.wethinkcode.co.za>
2020-06-23 11:25:53 +0200

    adding ex06

---
+++ ex05/ft_print_combo2.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_combo2(void)
+{
+	char a;
+	char b;
+
+	a ='0';
+	while (a <= '8')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			ft_putchar(a);
+			ft_putchar(b);
+			if (a != '8')
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	
+}
+
+int main()
+{
+	ft_print_combo2();
+	return (0);
+}
---
+++ ex06/ft_putchar.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int ft_putchar(char c);
+
+void ft_putnbr(int nb)
+{
+	char p;
+
+	p=1;
+	while (p <= 100)
+	{
+		ft_putchar(p);
+		p++;
+	}
+}
+
+int main()
+{
+	ft_putnbr(int nb);
+	return (0);
+}
